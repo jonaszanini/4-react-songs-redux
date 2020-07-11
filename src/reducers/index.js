daa66b88e85efe595ec423590cac2b7f8a696255
Jonas Zanini <jonaszanini@gmail.com>
2020-07-11 17:57:47 -0400

    Implementing redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
     return [
         { title: "Intentions", duration: "4:30" },
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
     return selectedSong;
 };
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
