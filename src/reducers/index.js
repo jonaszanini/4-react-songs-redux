0e2eae204d8cd1631bdc010a602f959dcecfe21e
Jonas Zanini <jonaszanini@gmail.com>
2020-07-11 13:35:02 -0400

    First commit

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+    return [
+        { title: "Intentions", duration: "4:30" },
+        { title: "Feeling good", duration: "5:00" },
+        { title: "Intentions", duration: "4:30" },
+        { title: "Any other", duration: "3:40" },
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === "SONG_SELECTED") {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
